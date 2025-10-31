c0ca1c0d187104ec0537d54ff3c2f4a8d2861d65
pyramidsmartsales-hue <pyramids.mart.sales@gmail.com>
2025-11-01 02:24:07 +0300

    Update socket.js

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,2 +1,11 @@
+// client/src/services/socket.js
 import { io } from "socket.io-client";
-export default io(import.meta.env.VITE_API_URL || "/");
+
+const API = import.meta.env.VITE_API_URL || window.location.origin;
+const socket = io(API, {
+  path: "/socket.io",
+  withCredentials: true,
+  transports: ["websocket","polling"]
+});
+
+export default socket;
